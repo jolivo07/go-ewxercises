7e89ac275216f0ecd5602cf377d40ab4e1ad7bc0
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-06-13 11:13:21 -0500

    calculator finish

---
+++ calculator/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	
+
+	"github.com/go-exercises/calculator/operation"
+)
+
+
+
+
+func main(){
+	operation.Operation{3,2}.Result()
+	
+}
+
+
+
+
---
+++ calculator/operation/operations.go
@@ -0,0 +1,31 @@
+package operation
+
+import "fmt"
+
+type Operation struct{
+	Num1 int
+	Num2 int
+}
+
+
+
+func (c Operation) Addition()int{
+	return c.Num1 + c.Num2
+}
+func (c Operation) Subtraction()int{
+	return c.Num1 - c.Num2
+}
+func (c Operation) Multiplication()int{
+	return c.Num1 * c.Num2
+}
+func (c Operation) Division()int{
+	return c.Num1 / c.Num2
+}
+
+func (c Operation) Result() {
+	 fmt.Printf("Addition: %d \nSubtraction: %d \nMultiplication: %d \nDivision: %d \n", c.Addition(), c.Subtraction(), c.Multiplication(), c.Division())
+
+}
+
+
+
