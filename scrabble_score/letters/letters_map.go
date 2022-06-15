4fb38b7877d391694deccce4836bac78fbfe31b8
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-06-15 15:14:29 -0500

    add file letters_map

---
+++ scrabble_score/letters/letters_map.go
@@ -0,0 +1,33 @@
+package letters 
+
+func LettersMap() map[string]int {
+	var letters = make(map[string]int)
+	letters["a"] = 1
+	letters["e"] = 1
+	letters["i"] = 1
+	letters["o"] = 1
+	letters["u"] = 1
+	letters["l"] = 1
+	letters["n"] = 1
+	letters["r"] = 1
+	letters["s"] = 1
+	letters["t"] = 1
+	letters["d"] = 2
+	letters["g"] = 2
+	letters["b"] = 3
+	letters["c"] = 3
+	letters["m"] = 3
+	letters["p"] = 3
+	letters["f"] = 4
+	letters["h"] = 4
+	letters["w"] = 4
+	letters["y"] = 4
+	letters["v"] = 4
+	letters["k"] = 5
+	letters["j"] = 8
+	letters["x"] = 8
+	letters["q"] = 10
+	letters["z"] = 10
+
+	 return letters
+}
--- scrabble_score/letters/score_for.go
+++ scrabble_score/letters/score_for.go
@@ -8,30 +8,18 @@ import (
 )
 
 func ScoreFor() {
-	var letters = make(map[int][]string)
-	letters[1] = []string{"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"}
-	letters[2] = []string{"d", "g"}
-	letters[3] = []string{"b", "c", "m", "p"}
-	letters[4] = []string{"f", "h", "w", "v", "y"}
-	letters[5] = []string{"k"}
-	letters[8] = []string{"j", "x"}
-	letters[10] = []string{"q", "z"}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	letterScanner := scanner.Text()
-	arrayLetters := strings.Split(letterScanner, "")
+	arrayLetters := strings.Split(strings.ToLower(letterScanner), "")
 
 	sum := 0
 	for _, valueArrayLetters := range arrayLetters {
-
-		for key, v := range letters {
-
-			for _, value := range v {
-				if value == valueArrayLetters {
-					sum = sum + key
-				}
+		for key, v := range LettersMap() {
+			if valueArrayLetters == key {
+				sum = sum + v
 			}
-
 		}
 	}
 	fmt.Println(sum)
